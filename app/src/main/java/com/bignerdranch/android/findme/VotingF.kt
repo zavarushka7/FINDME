b3fdb39659215ce057166c0909b78382dc469820
zavarushka7 <zavarushka7@gmail.com>
2024-06-10 16:23:02 +0500

    Fixed a bug in build.gradle and added the display of avatars in VotingF

--- app/src/main/java/com/bignerdranch/android/findme/VotingF.kt
+++ app/src/main/java/com/bignerdranch/android/findme/VotingF.kt
@@ -1,3 +1,12 @@
 package com.bignerdranch.android.findme
 //Голосование в режиме "Друзья", в котором игроки угадывают кому могут принадлежать эти 2 ответа.
 // Баллы начисляются тебе, если ты угадал правильно кому принадлежит ответ.
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VotingF : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_voting)
+    }
+}
